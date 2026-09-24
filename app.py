4421073d4191836c2d62a6ca2eb4fb4b83a37eed
alkruimi <126067011+alkruimi@users.noreply.github.com>
2026-09-24 06:24:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,8 @@ if st.button("🚀 ابدأ بناء الفيلم", type="primary", use_containe
             
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-3.6-flash
+",
                     contents=prompt_text
                 )
                 
